d0decbed79b062baf6c169d48e07fd3ecc3cec65
amelia <ameliawalcek@gmail.com>
2021-01-11 15:06:51 +0200

    shortened HTML file

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -1,16 +1,12 @@
 $(document).ready(function() {
-	
-    // Social Media Collapse
 	$('#sm-close').click(function(){
-		$('.s-media').addClass('sm-collapse');
-        $('#sm-open').delay(300).css('left', '0');
-	});
+		$('.s-media').addClass('sm-collapse')
+        $('#sm-open').delay(300).css('left', '0')
+	})
 
     $('#sm-open').click(function(){
-        $('#sm-open').css('left', '-60px');
-        $('.s-media').removeClass('sm-collapse');
+        $('#sm-open').css('left', '-60px')
+        $('.s-media').removeClass('sm-collapse')
         
-    });
-
-    // END Click Function
-});// END DOCUMENT.Ready
+    })
+})
